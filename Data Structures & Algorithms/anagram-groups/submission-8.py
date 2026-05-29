bd418c04492fc52898568336073a7b0cd8b1c01b
Andrew Ho <97128418+andrewzho@users.noreply.github.com>
2026-05-29 11:29:37 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        word_dictionary = dict()
+        for i in strs:
+            value = ascii(sorted(i))
+            if value in word_dictionary.keys():
+                word_dictionary[value].append(i)
+            else:
+                word_dictionary[value] = [i]
+        return list(word_dictionary.values())
+        
