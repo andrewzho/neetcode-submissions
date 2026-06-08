25727a2531f49d93812dcafaaf0127f94080a06f
Andrew Ho <97128418+andrewzho@users.noreply.github.com>
2026-06-07 22:54:16 -0700

    Add: two-integer-sum-ii - submission-23

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-23.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        head = 0
+        tail = len(numbers) - 1
+
+        while head <= tail:
+            val = numbers[head] + numbers[tail]
+            if val < target:
+                head += 1
+            elif val > target:
+                tail -= 1
+            else:
+                return [head + 1, tail + 1]
